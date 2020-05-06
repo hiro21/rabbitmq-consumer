af5d4644eec0350fcdbb63e99e6a2e414a7cbb6c
hiro21 <hiswing8@gmail.com>
2020-05-06 17:35:04 +0900

    DLX for manual

--- src/main/java/com/course/rabbitmqconsumer/consumer/MyPictureImageConsumer.java
+++ src/main/java/com/course/rabbitmqconsumer/consumer/MyPictureImageConsumer.java
@@ -2,9 +2,11 @@ package com.course.rabbitmqconsumer.consumer;
 
 import com.course.rabbitmqconsumer.entity.Picture;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -17,13 +19,21 @@ public class MyPictureImageConsumer {
     private static final Logger log = LoggerFactory.getLogger(MyPictureImageConsumer.class);
 
     @RabbitListener(queues = "q.mypicture.image")
-    public void listen(String message) throws IOException {
-        var p = objectMapper.readValue(message, Picture.class);
+    //public void listen(String message) throws IOException {
+    public void listen(Message message, Channel channel) throws IOException {
+        //var p = objectMapper.readValue(message, Picture.class);
+        var p = objectMapper.readValue(message.getBody(), Picture.class);
 
         if (p.getSize() > 9000) {
-            throw new AmqpRejectAndDontRequeueException("picture size too large:" + p.getSize());
+            // 例外スローではなく手動で実施する方法
+            //throw new AmqpRejectAndDontRequeueException("picture size too large:" + p.getSize());
+            channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
+
         }
         log.info("On image:{}", p.toString());
+
+        // RabbitMQにプロセスが完了したことを伝える必要がある。
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
 
     }
 }
