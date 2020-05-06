89c185c82759dd15aef0883e4336c6008d0b5bc8
hiro21 <hiswing8@gmail.com>
2020-05-06 12:05:15 +0900

    use topic exchange

---
+++ src/main/java/com/course/rabbitmqconsumer/consumer/PictureLogConsumer.java
@@ -0,0 +1,24 @@
+package com.course.rabbitmqconsumer.consumer;
+
+import com.course.rabbitmqconsumer.entity.Picture;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class PictureLogConsumer {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final Logger log = LoggerFactory.getLogger(PictureLogConsumer.class);
+
+    @RabbitListener(queues = "q.picture.log")
+    public void listen(String message) throws IOException {
+        var p = objectMapper.readValue(message, Picture.class);
+        log.info("On log:{}", p.toString());
+
+    }
+}
